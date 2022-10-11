021ad146b00166a8b555568e356761002c5715eb
uddinmayraj <mayrajuddin03@gmail.com>
2022-10-12 02:13:41 +0600

    fixed problem

--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -1,8 +1,9 @@
 // import React, { useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Toaster } from 'react-hot-toast';
 import { useLoaderData } from 'react-router-dom';
 import Quiz from '../Quiz/Quiz';
+import './Quizes.css'
 
 const Quizes = () => {
     const { data } = useLoaderData();
@@ -11,11 +12,22 @@ const Quizes = () => {
         <div>
             <Container>
                 <h3 className='text-center py-3 fs-3 text-capitalize my-3'> quiz of {name}</h3>
-                <Row className='g-4'>
-                    {
-                        questions.map(qus => <Quiz key={qus.id} question={qus}></Quiz>)
-                    }
-                    <Toaster></Toaster>
+                <Row>
+                    <Col sm={10}>
+                        <Row className='g-4'>
+                            {
+                                questions.map(qus => <Quiz key={qus.id} question={qus}></Quiz>)
+                            }
+                            <Toaster></Toaster>
+                        </Row>
+                    </Col>
+                    <Col sm={2}>
+                        <div className='count-div'>
+                            <p> Total Qus : {questions.length}</p>
+                            {/* <p>Correct Ans : </p>
+                            <p>Wrong Ans : </p> */}
+                        </div>
+                    </Col>
                 </Row>
             </Container>
         </div>
