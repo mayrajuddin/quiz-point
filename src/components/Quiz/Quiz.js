f0505daab480c42beb09bc461523c015c4da02f4
uddinmayraj <mayrajuddin03@gmail.com>
2022-10-12 18:03:07 +0600

    chnaging toast to div in correct ans

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Quiz.css';
 import { Card, Col, Form } from 'react-bootstrap';
 import { EyeIcon } from '@heroicons/react/24/solid';
@@ -9,11 +9,7 @@ const Quiz = (props) => {
     const { question } = props;
     const { options, correctAnswer, id } = question;
     console.log(props);
-
-    const notify = (answer) => toast(answer);
-    const showAns = ans => {
-        notify(ans)
-    }
+    const [open, setOpen] = useState(false)
     const ans = (value) => {
         if (value) {
             toast.success('correct answer')
@@ -34,9 +30,13 @@ const Quiz = (props) => {
                 <Card.Body>
                     <div className='d-flex justify-content-between align-items-center'>
                         <h2 className='fs-5 text-dark'> {question.question}</h2>
-                        <Link onClick={() => showAns(correctAnswer)} className='show-icon'> <EyeIcon className='icon'></EyeIcon></Link>
+                        <Link onClick={() => setOpen(!open)} className='show-icon'> <EyeIcon className='icon'>
+                        </EyeIcon></Link>
                     </div>
                     <Form>
+                        <div className={`${open ? 'd-block' : 'd-none'} ans-div`}>
+                            <p className='ms-2 p-2 py-3 text-center'> Correct Ans is : <span className='text-bold'>{correctAnswer}</span></p>
+                        </div>
                         {
                             options.map((option) => (
                                 <div key={option} className='option-div  border-0 my-3 rounded d-flex ' id={id}>
